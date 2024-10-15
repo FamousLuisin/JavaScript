c0057b9c0040cf9c883734c6196e9bd8e714a8a2
Luís Filipe <lufimelo@outlook.com>
2024-10-15 16:38:07 -0300

    Estudando fetch e axios

---
+++ ajax/server.js
@@ -0,0 +1,24 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+
+app.use(express.static('.'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+app.get('/teste', (req, res) => res.send('ok'))
+app.listen(8080, () => console.log("Executando..."))
+
+app.post('/formulario', (req, res) => {
+    res.send({
+        ...req.body,
+        id: 1
+    })
+})
+
+app.get('/parOuImpar', (req, res) => {
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+})
