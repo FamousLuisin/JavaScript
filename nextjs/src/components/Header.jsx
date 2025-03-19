0173322cf2a3945ea2a43da2ef06a4f320995858
Luís Filipe Melo <67298012+FamousLuisin@users.noreply.github.com>
2025-03-19 15:36:55 -0300

    Delete nextjs directory

--- nextjs/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import style from "@/app/styles/Header.module.css"
-
-export default function Header(props){
-    return (
-        <header className={style.header}>
-            <h1>{props.titulo}</h1>
-        </header>
-    )
-}
